aa1aa5930d47138cbdcb5d0b0ce9995a9e456e6b
1995046 <1995046@LI-S024-0T14>
2019-11-22 14:21:39 -0500

    Last push

--- src/main/AppCtr.java
+++ src/main/AppCtr.java
@@ -5,6 +5,7 @@ public class AppCtr {
     public static void main(String[] args) {
         System.out.println("Bonjour, " + args[0] + ", " + args[1]);
         System.out.println("Au revoir!");
+        System.out.println("Merci.");
     }
     
 }
