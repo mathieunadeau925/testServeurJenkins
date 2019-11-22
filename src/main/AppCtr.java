c8399432a5a78ce1af55316535293cb5b439020b
1995046 <1995046@LI-S024-0T14>
2019-11-22 14:07:07 -0500

    First Push

--- src/main/AppCtr.java
+++ src/main/AppCtr.java
@@ -3,7 +3,8 @@ package main;
 public class AppCtr {
 
     public static void main(String[] args) {
-        System.out.println("Bonjour" + args[0] + ", " + args[1]);
+        System.out.println("Bonjour, " + args[0] + ", " + args[1]);
+        
     }
     
 }
