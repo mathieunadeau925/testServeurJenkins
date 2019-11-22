efc80032242785ddc1b2dce82c4ce50152ec8aa9
1995046 <1995046@LI-S024-0T14>
2019-11-22 14:18:39 -0500

    second push

--- src/main/AppCtr.java
+++ src/main/AppCtr.java
@@ -4,7 +4,7 @@ public class AppCtr {
 
     public static void main(String[] args) {
         System.out.println("Bonjour, " + args[0] + ", " + args[1]);
-        
+        System.out.println("Au revoir!");
     }
     
 }
